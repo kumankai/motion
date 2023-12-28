94661e69c2f21540182b46ffd14d474ce62b2534
yaboi <anggaj@shernet.sheridancollege.ca>
2023-12-28 17:00:39 -0600

    added routes to add

--- api/account/src/routes/routes.js
+++ api/account/src/routes/routes.js
@@ -8,6 +8,32 @@ const userActions = require('../controllers/account-actions');
 
 const router = express.Router();
 
-// Routes go here
+/**
+ * GET / and /main routes for retrieving data of users
+ */
+
+/**
+ * POST /changepassword
+ */
+
+/**
+ * POST /deleteaccount
+ */
+
+/**
+ * POST /updateincome
+ */
+
+/**
+ * POST /updateexpense
+ */
+
+/**
+ * POST /updatehistory
+ */
+
+/**
+ * POST /changepassword
+ */
 
 module.exports = router;
