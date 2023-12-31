8202c28807d3376d89563c25936fab8e2ed2273d
yaboi <anggaj@shernet.sheridancollege.ca>
2023-12-30 19:32:29 -0700

    account routes

--- api/account/src/routes/routes.js
+++ api/account/src/routes/routes.js
@@ -8,32 +8,139 @@ const userActions = require('../controllers/account-actions');
 
 const router = express.Router();
 
-/**
- * GET / and /main routes for retrieving data of users
- */
+// **GET** / and /main
+// - Read all User data from MySQL
+// - send to client
 
-/**
- * POST /changepassword
- */
+// **POST** /changepassword
+// body = {
+//     userID
+//     newPassword
+// }
+// - Update in Account DB using userID
+// - Verify success to client
 
-/**
- * POST /deleteaccount
- */
+// **POST** /deleteaccount
+// body = {
+//     userID
+//     password
+// }
+// - Validate password
+// - Delete User in RefreshTokens DB
+// - Delete User in Accounts DB
+// - Delete User in mongoDB
+// - Verify success to client
 
-/**
- * POST /updateincome
- */
+// **POST** /addcard
+// body = {
+//     userID
+// }
+// - Save in Accounts DB
+// - Return cardID
 
-/**
- * POST /updateexpense
- */
+// **POST** /addincome
+// body = {
+//     userID
+//     cardID
+//     value
+// }
+// - Save in Accounts DB
+// - Return incomeID
 
-/**
- * POST /updatehistory
- */
+// **POST** /addexpense
+// body = {
+//     userID
+//     cardID
+//     value
+// }
+// - Save in Accounts DB
+// - Return expenseID
 
-/**
- * POST /changepassword
- */
+// **POST** /addhistory
+// body = {
+//     userID
+//     cardID
+//     value
+// }
+// - Insert in Accounts DB
+// - Return transID
+
+// **PUT** /editincome
+// body = {
+//     userID
+//     cardID
+//     incomeID
+//     {
+//         attribute
+//         value
+//     }
+// }
+// Possible attritbutes: "name", "amount", "datetime"
+// - Update Accounts DB
+// - Verify success
+
+// **PUT** /editexpense
+// body = {
+//     userID
+//     cardID
+//     expenseID
+//     {
+//         attribute
+//         value
+//     }
+// }
+// Possible attritbutes: "name", "amount", "datetime"
+// - Update Accounts DB
+// - Verify success
+
+// **PUT** /edithistory
+// body = {
+//     userID
+//     cardID
+//     transID
+//     {
+//         attribute
+//         value
+//     }
+// }
+// Possible attritbutes: "name", "amount", "datetime"
+// - Update Accounts DB
+// - Verify success
+
+// **POST** /deleteincome
+// body = {
+//     userID,
+//     cardID,
+//     incomeID
+// }
+// - Delete in Accounts DB
+// - Verify success
+
+// **POST** /deleteexpense
+// body = {
+//     userID,
+//     cardID,
+//     expenseID
+// }
+// - Delete in Accounts DB
+// - Verify success
+
+// **POST** /deletehistory
+// body = {
+//     userID,
+//     cardID,
+//     transID
+// }
+// - Delete in Accounts DB
+// - Verify success
+
+// **POST** /deletecard
+// body = {
+//     userID
+//     cardID
+// }
+// - Delete in Accounts DB
+// - Verify success
+
 
 module.exports = router;
